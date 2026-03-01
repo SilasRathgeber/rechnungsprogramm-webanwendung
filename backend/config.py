54a47afea1fc03cde5cc5a756401fd4aa208d520
silas.rathgeber@kabelmail.de <silas.rathgeber@kabelmail.de>
2026-03-01 19:29:08 +0100

    chore: remove outdated files and reorganize project structure

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-db_path = os.path.join(basedir, "../rechnungsprogramm_database.db")
+db_path = os.path.join(basedir, "/home/pi/daten/rechnungsprogramm_database.db")
 
 
 
